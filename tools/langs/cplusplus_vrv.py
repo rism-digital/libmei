c4981acd5ad0e1a3e013e383f5f3ccad8b5540cf
Klaus Rettinghaus <klaus.rettinghaus@gmail.com>
2022-11-08 19:10:44 +0100

    fix typos

--- tools/langs/cplusplus_vrv.py
+++ tools/langs/cplusplus_vrv.py
@@ -84,7 +84,7 @@ ENUM_GRP_END = """    ATT_CLASS_max
 """
 
 #
-# These templates the type definintions
+# These templates generate the type definitions
 #
 
 TYPE_GRP_START = """
@@ -212,7 +212,7 @@ CONVERTER_IMPL_TEMPLATE_END = """
 
 
 #
-# These templates generate a module level static method for setting attribute on an unspcified Object
+# These templates generate a module level static method for setting attribute on an unspecified Object
 #
 
 SETTERS_IMPL_TEMPLATE_START = """bool Att::Set{moduleNameCap}(Object *element, const std::string &attrType, const std::string &attrValue)
@@ -240,7 +240,7 @@ SETTERS_IMPL_TEMPLATE_END = """
 """
 
 #
-# These templates generate a module level static method for getting attributes of an unspcified Object
+# These templates generate a module level static method for getting attributes of an unspecified Object
 #
 
 GETTERS_IMPL_TEMPLATE_START = """void Att::Get{moduleNameCap}(const Object *element, ArrayOfStrAttr *attributes)
